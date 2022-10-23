d585db39f4fde6b4b4b46dd7bfaefb4b2a344d07
Lucas@311090 <lucas.pradella@hotmail.com>
2022-10-23 20:26:06 -0300

    primeiro commit

---
+++ IaC-curso1/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-017fecd1353bcc96e"
+  instance_type = "t2.micro"
+
+  key_name = "iac-oregon"
+
+  tags = {
+    Name = "primeira-instancia"
+  }
+}
